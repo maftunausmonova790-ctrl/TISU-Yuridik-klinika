4c314555968089d57d9e3bcfe1ea888ea898a0a2
maftunausmonova790-ctrl <maftunausmonova790@gmail.com>
2026-04-24 13:55:17 +0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from handlers.main import main_router
-from handlers.questions import questions_router
-from handlers.applications import applications_router
-from handlers.admin import admin_router
-
-__all__ = ["main_router", "questions_router", "applications_router", "admin_router"]
